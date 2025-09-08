aa8677479876ce77df3ec2f12be3ee83c365f475
Canmi <9997200@qq.com>
2025-09-08 21:48:41 +0800

    feat: implement graceful shutdown handler

---
+++ src/shutdown.rs
@@ -0,0 +1,32 @@
+/* src/shutdown.rs */
+
+use fancy_log::{LogLevel, log};
+
+pub async fn signal_handler() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    log(
+        LogLevel::Info,
+        "Signal received, starting graceful shutdown...",
+    );
+}
